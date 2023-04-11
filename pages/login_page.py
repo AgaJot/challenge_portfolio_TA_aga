fce1ebd6bf5cdbb6d19b2bb055a026a5ecc29b93
AgaJot <116113886+AgaJot@users.noreply.github.com>
2023-04-11 22:38:11 +0200

    Selectors tasks

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,10 +1,15 @@
-# from pages.base_page import BasePage
-#
-#
-# class LoginPage(BasePage):
-#     login_field_xpath = "//*[@id='login']"
-#     password_field_xpath =
-#     sign_in_button_xpath =
-#
-#     def type_in_email(self, email):
-#         self.field_send_keys(self.login_field_xpath, email)
+from pages.base_page import BasePage
+
+
+class LoginPage(BasePage):
+    login_field_xpath = "//*[@id='login']"
+    password_field_xpath = "//*[@id='password']"
+    sign_in_button_xpath = "//*[text()='Sign in']"
+    remind_password_button_xpath = "//*[text()='Remind password']"
+    english_button_xpath = "//*[text()='English']"
+    polski_button_xpath = "//*[text()='Polski']"
+    scouts_panel_xpath = "/html/body/div/form/div/div[1]/h5"
+
+
+    def type_in_email(self, email):
+        self.field_send_keys(self.login_field_xpath, email)
